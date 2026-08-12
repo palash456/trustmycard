b49b84863e7772f63b119ecec8e91d0de265319e
palash456 <palashsoni456@gmail.com>
2026-08-13 01:23:37 +0530

    Remove 'CompleteRegistration' tracking from MetaPixel component for streamlined event tracking

--- frontend/website/src/components/MetaPixel.tsx
+++ frontend/website/src/components/MetaPixel.tsx
@@ -11,7 +11,6 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '${META_PIXEL_ID}');
 fbq('track', 'PageView');
-fbq('track', 'CompleteRegistration');
 `;
 
 export function MetaPixel() {
